f9bfbd5efcb7a5c6703ca1097057386e99ea34d9
Sagar Patil <sagarreddypatil@gmail.com>
2022-08-18 00:05:05 +0530

    a few changes

---
+++ .config/i3/libinput.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+xinput --set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
+xinput --set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
+"""
+
+import os
+
+def strval(val):
+    return f'"{val}"'
+
+def xinput_cmd(device, option, value):
+    if type(value) is list:
+        value = ", ".join([str(val) for val in value])
+
+    os.system(f'xinput --set-prop "{device}" "libinput {option}" {value}')
+
+touchpad = "Elan Touchpad"
+
+xinput_cmd(touchpad, "Tapping Enabled", 1)
+xinput_cmd(touchpad, "Natural Scrolling Enabled", 1)
+xinput_cmd(touchpad, "Click Method Enabled", [0, 1])
