23872c9468412b098185447bf623267dac44f2d5
sagarreddypatil <sagarreddypatil@gmail.com>
2023-05-18 22:16:05 -0400

    make the redshift script more better

--- .config/i3/redshift.sh
+++ .config/i3/redshift.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+until curl -s -o /dev/null "http://example.com"; do
+  continue
+done
+
 redshift-gtk -l $(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | awk 'OFS=":" {print $3,$5}' | tr -d ',}') -t 6500:3000
