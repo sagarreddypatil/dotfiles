3b2f5085d5c9025a2d2bffcc0086ffe80ab98046
Sagar Patil <spatil@asylonrobotics.com>
2024-06-21 12:42:20 -0400

    small changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,15 +2,14 @@
 
 killall -q polybar
 
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload mybar &
-#   done
-# else
-#   polybar --reload mybar &
-# fi
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
-polybar --reload mybar &
-
+# polybar --reload mybar &
 # polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 
